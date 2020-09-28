20f790aa5deba8470578894608c216885fc012e6
xinshiyou <xinshiyou@IIGS-A7290-D.iqiyi.pps>
2020-09-28 17:18:22 +0800

    add debug message: System.out

--- ratis-examples/src/main/java/org/apache/ratis/examples/counter/server/CounterServer.java
+++ ratis-examples/src/main/java/org/apache/ratis/examples/counter/server/CounterServer.java
@@ -21,9 +21,12 @@ package org.apache.ratis.examples.counter.server;
 import org.apache.ratis.conf.RaftProperties;
 import org.apache.ratis.examples.counter.CounterCommon;
 import org.apache.ratis.grpc.GrpcConfigKeys;
+import org.apache.ratis.protocol.RaftGroupId;
 import org.apache.ratis.protocol.RaftPeer;
 import org.apache.ratis.server.RaftServer;
 import org.apache.ratis.server.RaftServerConfigKeys;
+import org.apache.ratis.server.impl.RaftServerImpl;
+import org.apache.ratis.server.impl.RaftServerProxy;
 import org.apache.ratis.util.NetUtils;
 
 import java.io.File;
@@ -33,56 +36,71 @@ import java.util.Scanner;
 
 /**
  * Simplest Ratis server, use a simple state machine {@link CounterStateMachine}
- * which maintain a counter across multi server.
- * This server application designed to run several times with different
- * parameters (1,2 or 3). server addresses hard coded in {@link CounterCommon}
+ * which maintain a counter across multi server. This server application
+ * designed to run several times with different parameters (1,2 or 3). server
+ * addresses hard coded in {@link CounterCommon}
  * <p>
  * Run this application three times with three different parameter set-up a
  * ratis cluster which maintain a counter value replicated in each server memory
  */
 public final class CounterServer {
 
-  private CounterServer(){
-  }
+	private CounterServer() {
+	}
 
-  public static void main(String[] args) throws IOException, InterruptedException {
-    if (args.length < 1) {
-      System.err.println("Usage: java -cp *.jar org.apache.ratis.examples.counter.server.CounterServer {serverIndex}");
-      System.err.println("{serverIndex} could be 1, 2 or 3");
-      System.exit(1);
-    }
+	public static void main(String[] args) throws IOException, InterruptedException {
+		if (args.length < 1) {
+			System.err.println(
+					"Usage: java -cp *.jar org.apache.ratis.examples.counter.server.CounterServer {serverIndex}");
+			System.err.println("{serverIndex} could be 1, 2 or 3");
+			System.exit(1);
+		}
 
-    //find current peer object based on application parameter
-    RaftPeer currentPeer =
-        CounterCommon.PEERS.get(Integer.parseInt(args[0]) - 1);
+		// find current peer object based on application parameter
+		RaftPeer currentPeer = CounterCommon.PEERS.get(Integer.parseInt(args[0]) - 1);
 
-    //create a property object
-    RaftProperties properties = new RaftProperties();
+		// create a property object
+		RaftProperties properties = new RaftProperties();
 
-    //set the storage directory (different for each peer) in RaftProperty object
-    File raftStorageDir = new File("./" + currentPeer.getId().toString());
-    RaftServerConfigKeys.setStorageDir(properties,
-        Collections.singletonList(raftStorageDir));
+		// set the storage directory (different for each peer) in RaftProperty object
+		File raftStorageDir = new File("./" + currentPeer.getId().toString());
+		RaftServerConfigKeys.setStorageDir(properties, Collections.singletonList(raftStorageDir));
 
-    //set the port which server listen to in RaftProperty object
-    final int port = NetUtils.createSocketAddr(currentPeer.getAddress()).getPort();
-    GrpcConfigKeys.Server.setPort(properties, port);
+		// set the port which server listen to in RaftProperty object
+		final int port = NetUtils.createSocketAddr(currentPeer.getAddress()).getPort();
+		GrpcConfigKeys.Server.setPort(properties, port);
 
-    //create the counter state machine which hold the counter value
-    CounterStateMachine counterStateMachine = new CounterStateMachine();
+		// create the counter state machine which hold the counter value
+		CounterStateMachine counterStateMachine = new CounterStateMachine();
 
-    //create and start the Raft server
-    RaftServer server = RaftServer.newBuilder()
-        .setGroup(CounterCommon.RAFT_GROUP)
-        .setProperties(properties)
-        .setServerId(currentPeer.getId())
-        .setStateMachine(counterStateMachine)
-        .build();
-    server.start();
+		// create and start the Raft server
+		RaftServer server = RaftServer.newBuilder().setGroup(CounterCommon.RAFT_GROUP).setProperties(properties)
+				.setServerId(currentPeer.getId()).setStateMachine(counterStateMachine).build();
+		server.start();
 
-    //exit when any input entered
-    Scanner scanner = new Scanner(System.in);
-    scanner.nextLine();
-    server.close();
-  }
+		Thread.sleep(1000 * 10);
+
+		final RaftServerProxy proxy = (RaftServerProxy) server;
+		RaftGroupId groupId = null;
+		server.getGroupIds().forEach(temp -> {
+			System.out.println("GroupID:" + temp + "\tserverID:" + server.getId());
+			if (null == groupId) {
+				RaftServerImpl raftServerImpl;
+				try {
+					raftServerImpl = proxy.getImpl(temp);
+					System.out.println("Is Leader ? ==>" + raftServerImpl.isAlive() + "\t" + raftServerImpl.isLeader()
+							+ "\t" + raftServerImpl.isFollower());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		System.out.println("INFOS:" + server.getId() + "\tState:" + server.getLifeCycleState() + "\tRPC:"
+				+ server.getRpcType().name());
+
+		// exit when any input entered
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine();
+		server.close();
+	}
 }
